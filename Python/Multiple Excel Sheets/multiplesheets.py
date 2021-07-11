7bbe174a760eb0c88e477d1d4580f4fff75a9719
אילון איילשטיין <eilon.eilstein@moh.gov.il>
2021-07-11 16:21:31 +0300

    Update Script by using helper functions

--- Python/Multiple Excel Sheets/multiplesheets.py
+++ Python/Multiple Excel Sheets/multiplesheets.py
@@ -2,54 +2,53 @@ import pandas as pd
 import os
 import glob
 
+
+def list_to_dict(lst):
+    new_dict = {}
+    for i in range(len(lst)):
+        new_dict[i+1] = lst[i]
+
+    return new_dict
+
+
+def print_dict(dict):
+    for key in dict.keys():
+        print(key, '-', dict[key])
+
+
 def multipleSheets():
 
     dir_path = input('Insert folder path: ')
 
     print('\nExcels in chosen folder: ')
-
-    files_dict = {}
-    i = 1
-    for filename in glob.glob(os.path.join(dir_path, '*.xlsx')):
-        # sliced_name = filename[(filename[::-1].find('\\'))*(-1):]
-        sliced_name = filename.replace(dir_path+'\\', "")
-        files_dict[i] = sliced_name
-        print(i, '-', sliced_name)
-        i += 1
-
+    # Get all Excel Files
+    filenames_lst = [filename.replace(dir_path+'\\', "") for filename in glob.glob(os.path.join(dir_path, '*.xlsx'))]
+    filenames_dict = list_to_dict(filenames_lst)
+    print_dict(filenames_dict)
     print()
-
+    # Choose Excel
     excel_number = int(input('Insert Excel NUMBER: '))
-
     print()
-    chosen_excel = files_dict[excel_number]
+    chosen_excel = filenames_dict[excel_number]
+
+    # Get all Excel Sheets
     path = dir_path +'\\'+ chosen_excel
-
     print('\nSheets in chosen Excel: ')
-
     xl_sheets = pd.ExcelFile(path).sheet_names
-    sheets_dict = {}
-    for i in range(1, len(xl_sheets)+1):
-        sheets_dict[i] = xl_sheets[i-1]
-        print(i, "-", xl_sheets[i-1])
-
+    sheets_dict = list_to_dict(xl_sheets)
+    print_dict(sheets_dict)
+    # Choose Sheet
     sheet_number = int(input('Insert Sheet NUMBER: '))
-
     print()
     chosen_sheet = sheets_dict[sheet_number]
-
+    
+    # Get all Sheet's Columns
     main_df = pd.read_excel(path, sheet_name=chosen_sheet)
-
     print('\nColumns in chosen Excel: ')
-
     cols_list = list(main_df.columns)
-
-    cols_dict = {}
-    for i in range(1, len(cols_list)+1):
-        cols_dict[i] = cols_list[i-1]
-        print(i, "-", cols_list[i-1])
-
-
+    cols_dict = list_to_dict(cols_list)
+    print_dict(cols_dict)
+    # Choose Column
     chosen_col_num = int(input('Choose column NUMBER to group by which: '))
     chosen_col = cols_dict[chosen_col_num]
     cats = list(main_df[chosen_col].unique())
@@ -58,7 +57,7 @@ def multipleSheets():
     output_path = dir_path+f'/{chosen_excel.replace(".xlsx", "")} by {chosen_col}.xlsx'
     writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
 
-
+    # Create new sheet for each of the chosen category's value
     for cat in cats:
         main_df[main_df[chosen_col] == cat].to_excel(writer, sheet_name=str(cat).replace('/', '').replace('\\', ''), index=False)
 
@@ -74,8 +73,9 @@ def main():
             ind = multipleSheets()
         except Exception as e:
             print("#######")
-            print(f"There are some errors{e}")
+            print(f"There are some errors {e}")
             print("#######")
+
 
 if __name__ == "__main__":
     main()
